8752f11c9b1d6ac3df07acf4196049742b0cb2f5
Tabaro Christian <tabarochristian@yahoo.com>
2023-12-23 00:01:28 +0800

    Update settings.py

--- poro/settings.py
+++ poro/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', SECRET_KEY)
 DEBUG = int(os.getenv('DEBUG', 1))
 print('DEBUG:', DEBUG)
 
-ALLOWED_HOSTS = ['https://poro.kaditaj.com']
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+ALLOWED_HOSTS = ['poro.kaditaj.com']
+CSRF_TRUSTED_ORIGINS = ['https://poro.kaditaj.com']
 
 #ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS', '*').split(','))
 
