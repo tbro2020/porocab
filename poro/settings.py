806b650a5fac199d90fc6337b6d9db94fd4de9f1
Tabaro Christian <tabarochristian@yahoo.com>
2024-01-07 09:53:24 +0100

    Update settings.py

--- poro/settings.py
+++ poro/settings.py
@@ -297,7 +297,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)],
+            "hosts": [(os.getenv("CELERY_BROKER_URL", 'redis://localhost:6379'))],
         },
     },
 }
