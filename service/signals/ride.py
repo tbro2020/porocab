3578cdf5dc40c3a4262713885c96aefcbc826128
Tabaro Christian <tabarochristian@yahoo.com>
2024-01-13 15:28:08 +0100

    Update ride.py

--- service/signals/ride.py
+++ service/signals/ride.py
@@ -6,6 +6,17 @@ from django.dispatch import receiver
 from service.models import Ride
 from core.tasks import drivers
 
+from djmoney.models.fields import MoneyField
+from django.conf import settings
+from djmoney.money import Money
+
+
+@receiver(signals.pre_save, sender=Ride)
+def pre_save_ride(sender, instance, **kwargs):
+    price = instance.duration_in_minutes*settings.PRICE_PER_MINUTE
+    price = Money(price, settings.DEFAULT_CURRENCY)
+    instance.cost = price
+
 @receiver(signals.post_save, sender=Ride)
 def post_save_ride(sender, instance, created, **kwargs):
     async_to_sync(get_channel_layer().group_send)('ride_{}'.format(instance.id), {
