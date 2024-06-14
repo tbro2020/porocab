ee38981e409963883f72c2d268dd554d69dbf524
Tabaro Christian <tabarochristian@yahoo.com>
2024-06-14 15:34:15 +0100

    onesignal notification

--- service/signals/ride.py
+++ service/signals/ride.py
@@ -1,3 +1,4 @@
+from onesignal.model.notification import Notification
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
@@ -6,7 +7,16 @@ from django.dispatch import receiver
 
 from service.utils import price_the_ride
 from service.tasks import drivers
-from service.models import Ride
+from service.models import Ride, Vehicle
+
+import onesignal
+from onesignal.api import default_api
+
+# Replace with your OneSignal App ID and REST API key
+ONESIGNAL_APP_ID = 'MzZhZmNiYWYtMTdmYS00NTg3LThhZGYtM2I3YzJhYzNmNGYz'
+ONESIGNAL_REST_API_KEY = 'ZTA2NzliMzItMjMxMS00NzRkLTg0ODMtZGYyOTU4MTdlZGY1'
+configuration = onesignal.Configuration(app_key = ONESIGNAL_APP_ID, user_key = ONESIGNAL_REST_API_KEY)
+
 
 @receiver(signals.pre_save, sender=Ride)
 def pre_save_ride(sender, instance, **kwargs):
@@ -19,6 +29,19 @@ def post_save_ride(sender, instance, created, **kwargs):
         'type': 'broadcast',
         'payload': instance.serialized
     })
+    if created:
+        busies = Ride.objects.filter(status__in=['accepted', 'started']).values_list('driver__id', flat=True)
+        drivers = Vehicle.objects.exclude(driver__in=busies).values_list('driver__id', flat=True)
+        try:
+            with onesignal.ApiClient(configuration) as api_client:
+                default_api.DefaultApi(api_client).create_notification(Notification(
+                    app_id=ONESIGNAL_APP_ID,
+                    include_external_user_ids=drivers,
+                    template_id="3a16cf8e-5be1-4482-b7c1-bb4d3395b6d0"
+                ))
+        except Exception as ex:
+            print(ex)
+
     if not created and instance.status != 'pending': return
     drivers.delay(instance.id)
         
