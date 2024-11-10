5b33b33cb904c5377d34e7791a26ae0d4969f012
Tabaro Christian <tabarochristian@yahoo.com>
2024-11-10 19:05:39 +0800

    new ride

--- core/signals/user.py
+++ core/signals/user.py
@@ -5,6 +5,11 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
+def create_auth_token(sender, instance, created, **kwargs):
     if not created: return
     Token.objects.create(user=instance)
+    
+    # set default password if empty or none
+    if not instance.password:
+        instance.set_password('Kinshasa-2021')
+        instance.save()
--- service/tasks/new_ride.py
+++ service/tasks/new_ride.py
@@ -16,11 +16,14 @@ pusher_client = pusher.Pusher(**{
 
 @shared_task
 def broadcast_new_ride_to_driver(ride_pk):
+    waiting_time = 5
     current, max = 0, 5
     event_name = 'ride'
 
     ride = Ride.objects.get(pk=ride_pk)
-    drivers = Vehicle.objects.filter(last_know_location__dwithin=(ride.pick_up_location, Distance(m=1000)))
+    # drivers = Vehicle.objects.filter(last_know_location__dwithin=(ride.pick_up_location, Distance(m=1000)))
+    drivers = Vehicle.objects.all()
+    
     while max >= current:
         if not drivers or ride.status != RideStatus.PENDING : break
         _drivers = drivers.values_list('driver', flat=True)
@@ -28,8 +31,9 @@ def broadcast_new_ride_to_driver(ride_pk):
         pusher_client.trigger([f'user-{driver}' for driver in _drivers], event_name, ride.serialized)
         drivers = drivers.all()
         ride.refresh_from_db()
-        time.sleep(5)
+        time.sleep(waiting_time)
         current += 1
+        
     # update ride status to end the broadcasting
     ride.status = RideStatus.CANCELLED
     ride.save()
